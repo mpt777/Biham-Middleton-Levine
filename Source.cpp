603009be2a52e73ee1fff997bcb58e2bb8d57dc3
Marshal <mptaylor777@gmail.com>
2022-03-16 19:22:57 -0600

    Bux fixes

--- Source.cpp
+++ Source.cpp
@@ -11,8 +11,8 @@
 #define WIDTH 700
 #define HEIGHT 700
 
-#define XCELLS 100
-#define YCELLS 100
+#define XCELLS 350
+#define YCELLS 350
 
 int cellw = WIDTH / XCELLS, cellh = HEIGHT / YCELLS;
 
@@ -21,72 +21,58 @@ int RED = 1;
 int BLUE = 2;
 
 bool stopped = false;
+bool step = true;
 
 using namespace std;
 
 std::vector<std::vector<int>> grid;
 
-sf::RenderWindow mwindow(sf::VideoMode(WIDTH, HEIGHT), "Conway's Game of Life");
+sf::RenderWindow mwindow(sf::VideoMode(WIDTH, HEIGHT), "Traffic");
 
-bool darkmode = false;
-
-int getneighbours(int ln, int col, std::vector<std::vector<int>> dgrid = grid) {
-    int c = 0;
-    for (int y = ln - 1; y <= ln + 1; y++) {
-        for (int x = col - 1; x <= col + 1; x++) {
-            if (y >= 0 && x >= 0 && y < YCELLS && x < XCELLS) {
-                c += dgrid[y][x];
-            }
-        }
-    }
-
-    return c - dgrid[ln][col];
-}
-
-float defaultdelay = 0.0001f;
+float defaultdelay = 1.f;
 float fastdelay = 0.0005f;
 float delay = defaultdelay;
 
 sf::Clock gameclock;
 void timemanager() {
     float elapsed = gameclock.getElapsedTime().asSeconds();
+    //if (elapsed >= delay) {
+    
+        std::vector<std::vector<int>> temp = grid;
 
-///    if (elapsed >= delay) {
-        cout << elapsed << endl;
-
-        for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
-            for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
-                if (grid[y_pos][x_pos] == RED) {
-                    if (grid[(y_pos + 1) % YCELLS][x_pos] == EMPTY) {
-                        grid[(y_pos + 1) % YCELLS][x_pos] = RED;
-                        grid[y_pos][x_pos] = EMPTY;
+        for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+            for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
+                if (temp[x_pos][y_pos] == RED) {
+                    if (temp[x_pos][(y_pos + 1) % YCELLS] == EMPTY) {
+                        grid[x_pos][(y_pos + 1) % YCELLS] = RED;
+                        grid[x_pos][y_pos] = EMPTY;
                     }
                 }
             }
         }
-        for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
-            for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
-                if (grid[y_pos][x_pos] == BLUE) {
-                    if (grid[(y_pos)][(x_pos + 1) % XCELLS] == EMPTY) {
-                        grid[(y_pos)][(x_pos + 1) % XCELLS] = BLUE;
-                        grid[y_pos][x_pos] = EMPTY;
+        temp = grid;
+        for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+            for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
+                if (temp[x_pos][y_pos] == BLUE) {
+                    if (temp[(x_pos + 1) % XCELLS][y_pos] == EMPTY) {
+                        grid[(x_pos + 1) % XCELLS][y_pos] = BLUE;
+                        grid[x_pos][y_pos] = EMPTY;
                     };
                 }
             }
         }
-
- //       gameclock.restart();
- //   }
+        gameclock.restart();
+   // }
 }
 
 void initgrid() {
-    srand(time(NULL));
+//    srand(time(NULL));
     for (int yc = 0; yc < YCELLS; yc++) {
         std::vector<int> ln;
         for (int xc = 0; xc < XCELLS; xc++) {
             int random = rand() % 3 + 0;
             if (random != 0) {
-                if ((rand() % 10 + 0)) {
+                if ((rand() % 3 + 0)) {
                     random = 0;
                 }
             }
@@ -160,10 +146,7 @@ int main(int argc, char** argv) {
                     cleargrid();
                 }
                 else if (event.key.code == sf::Keyboard::Tab) { // fast forward
-                    if (delay != fastdelay) {
-                        delay = fastdelay;
-                        mwindow.setTitle("Conway's Game of Life - Fast Forward");
-                    }
+                    
                 }
             }
             else if (event.type == sf::Event::KeyReleased) {
@@ -224,14 +207,12 @@ int main(int argc, char** argv) {
         }
 
         /* Draw the cells */
-        for (int ln = 0; ln < YCELLS; ln++) {
-            std::vector<int> l = grid[ln];
-            for (int cn = 0; cn < XCELLS; cn++) {
-                int col = l[cn];
-
+        for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+            for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
+                int col = grid[x_pos][y_pos];
                 if (col != EMPTY) {
                     sf::RectangleShape cell;
-                    cell.setPosition(cn * cellw + th, ln * cellh + th);
+                    cell.setPosition(y_pos * cellw + th, x_pos * cellh + th);
                     cell.setSize(sf::Vector2f(cellw - th, cellh - th));
 
                     cell.setOutlineThickness(0);
