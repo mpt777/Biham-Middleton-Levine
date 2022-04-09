3c30ca6639e7f36e64c4e4349bccfeaccc51e87e
Marshal <mptaylor777@gmail.com>
2022-04-08 20:01:32 -0600

    MPI passing

--- Source.cpp
+++ Source.cpp
@@ -159,7 +159,6 @@ int** alloc_2d_int(int rows, int cols) {
     return array;
 }
 
-
 int main(int argc, char** argv) {
 
     sf::Color gridcolor(200, 200, 200);
@@ -182,13 +181,36 @@ int main(int argc, char** argv) {
     togglestop();
     cleargrid();
 
-    int** A;
+    int x_cells = XCELLS / (size - 1);
 
-    A = alloc_2d_int(XCELLS, YCELLS);
+    int** A;
+    int** recv_A;
+
+    int flag;
+
+    int next_in_line = (rank + 1) % size;
+    if (next_in_line == 0) {
+        next_in_line = 1;
+    }
+
+    int prev_in_line = rank - 1;
+    if (prev_in_line == 0) {
+        prev_in_line = size-1;
+    }
+
+    int token_tag = 7;
+    int ask_tag = 2;
+    int response_tag = 3;
+
+    int pending_requests = 0;
+    
+    cout << rank << " | next " << next_in_line << " | prev " << prev_in_line << endl;
 
     //grid[XCELLS][YCELLS];
 
     if (rank == 0) { /* i am a pig */
+        A = alloc_2d_int(XCELLS, YCELLS);
+        recv_A = alloc_2d_int(x_cells, YCELLS);
 
         std::cout << std::endl << "Biham–Middleton–Levine" << std::endl << std::endl
             << "Space: Toggle start/stop" << std::endl
@@ -197,24 +219,6 @@ int main(int argc, char** argv) {
 
 
         auto start = high_resolution_clock::now();
-
-
-        srand(time(NULL));
-        for (int yc = 0; yc < YCELLS; yc++) {
-            for (int xc = 0; xc < XCELLS; xc++) {
-                int random = rand() % 3 + 0;
-                if (random != 0) {
-                    if ((rand() % 2 + 0)) {
-                        random = 0;
-                    }
-                }
-                A[xc][yc] = random;
-            }
-        }
-
-        for (int i = 1; i < size; ++i) {
-            MPI_Send(&(A[0][0]), XCELLS * YCELLS, MPI_INT, i, 0, MCW);
-        }
 
         //while
         while (mwindow.isOpen()) {
@@ -264,6 +268,7 @@ int main(int argc, char** argv) {
 
             float th = 1.f;
 
+            ////////////////////////////////////////////////
             /* Draw the grid */
             for (int xc = 0; xc <= XCELLS; xc++) {
                 int c = cellw * xc;
@@ -290,9 +295,27 @@ int main(int argc, char** argv) {
 
                 mwindow.draw(line);
             }
+            /////////////////////////////////////////////////////
             if (!stopped) {
-                MPI_Send(&work, 1, MPI_INT, 1, 1, MCW);
-                MPI_Recv(&(A[0][0]), XCELLS * YCELLS, MPI_INT, 1, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+                MPI_Send(&work, 1, MPI_INT, (rank + 1) % size, token_tag, MCW);
+                
+                //cout << "wait" << endl;
+                MPI_Recv(&work, 1, MPI_INT, size-1, token_tag, MCW, &status);
+                //cout << "got" << endl;
+
+                for (int i = 1; i < size; i++) {
+                    MPI_Status status;
+                    //cout << "here for "<< i << endl;
+                    MPI_Recv(&(recv_A[0][0]), x_cells * YCELLS, MPI_INT, i, 0, MCW, &status);
+                    int sender = status.MPI_SOURCE;
+                    for (int x_pos = 0; x_pos < x_cells; x_pos++) {
+                        for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
+                            A[x_pos + ((i-1) * x_cells)][y_pos] = recv_A[x_pos][y_pos];
+                        }
+                    }
+                }
+                //cout << "render" << endl;
+                
                 //MPI_Recv(&work, XCELLS * YCELLS, MPI_INT, 1, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
             }
             /* Draw the cells */
@@ -319,29 +342,43 @@ int main(int argc, char** argv) {
 
             mwindow.display();
         }
-
-
-        //cout << "All Done!" << endl;
-
     }
     else {  
-        /* i am a horse  */
-        mwindow.close();
-        MPI_Recv(&(A[0][0]), XCELLS * YCELLS, MPI_INT, 0, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+        A = alloc_2d_int(x_cells, YCELLS);
 
-        
+        //Radnomly sets each board up
+        srand(time(NULL) + (rank));
+
+        for (int xc = 0; xc < x_cells; xc++) {
+            for (int yc = 0; yc < YCELLS; yc++) {
+                int random = rand() % 3 + 0;
+                if (random != 0) {
+                    if ((rand() % 2 + 0)) {
+                        random = 0;
+                    }
+                }
+                A[xc][yc] = random;
+            }
+        }
+
+        mwindow.close();
         while (1){
-            MPI_Recv(&work, 1, MPI_INT, 0, MPI_ANY_TAG, MCW, MPI_STATUS_IGNORE);
+            int data[3] = { -1,-1,-1 };
+            int new_data[3] = { -1,-1,-1 };
 
             //////////////////////////////////////////////////////////////
-            int temp[XCELLS][YCELLS];
-            for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+            vector<vector<int>> temp(x_cells, vector<int>(YCELLS, 0));
+
+            //Clones the current board into a temp board
+            for (int x_pos = 0; x_pos < x_cells; x_pos++) {
                 for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
                     temp[x_pos][y_pos] = A[x_pos][y_pos];
                 }
             }
 
-            for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+            /////////////////////////////////////////////////////////////
+
+            for (int x_pos = 0; x_pos < x_cells; x_pos++) {
                 for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
                     if (temp[x_pos][y_pos] == RED) {
                         if (temp[x_pos][(y_pos + 1) % YCELLS] == EMPTY) {
@@ -352,25 +389,105 @@ int main(int argc, char** argv) {
                 }
             }
 
-            for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+            /////////////////////////////////////////////////////////////
+            //Clones the current board into a temp board
+            for (int x_pos = 0; x_pos < x_cells; x_pos++) {
                 for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
                     temp[x_pos][y_pos] = A[x_pos][y_pos];
                 }
             }
+            //////////////////////////////////////////////////////////////
 
-            for (int x_pos = 0; x_pos < XCELLS; x_pos++) {
+
+            //////////////////////////////
+            /*
+            for (int x_pos = 0; x_pos < x_cells; x_pos++) {
                 for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
                     if (temp[x_pos][y_pos] == BLUE) {
-                        if (temp[(x_pos + 1) % XCELLS][y_pos] == EMPTY) {
-                            A[(x_pos + 1) % XCELLS][y_pos] = BLUE;
+                        if (temp[(x_pos+1) % x_cells][(y_pos)] == EMPTY) {
+                            A[(x_pos+1)%x_cells][y_pos] = BLUE;
                             A[x_pos][y_pos] = EMPTY;
                         }
                     }
                 }
             }
+            */
+            
+            for (int x_pos = 0; x_pos < x_cells; x_pos++) {
+                for (int y_pos = 0; y_pos < YCELLS; y_pos++) {
+                    if (temp[x_pos][y_pos] == BLUE) {
+                        if (x_pos + 1 >= x_cells) {
+
+                            data[0] = BLUE;
+                            data[1] = 0;
+                            data[2] = y_pos;
+
+                            //cout <<"Send Data Rank " << rank << " color " << data[0] << " x"<< data[1] << " y" << data[2] << endl;
+                            MPI_Send(&data, 3, MPI_INT, next_in_line, ask_tag, MCW);
+                            pending_requests++;
+                        }
+                        else if (temp[(x_pos + 1)][y_pos] == EMPTY) {
+                            A[(x_pos + 1)][y_pos] = BLUE;
+                            A[x_pos][y_pos] = EMPTY;
+                        }
+                    }
+                }
+            }
+
+            //Token passing
+            while (1) {
+                MPI_Iprobe(prev_in_line, 2, MCW, &flag, &status);
+                if (flag) {
+                    while (flag) {
+                        MPI_Recv(&data, 3, MPI_INT, prev_in_line, ask_tag, MCW, MPI_STATUS_IGNORE);
+                        int color = data[0];
+                        int x = data[1];
+                        int y = data[2];
+                        //cout << "Recv Data Rank " << rank << " color " << data[0] << " x" << data[1] << " y" << data[2] << endl;
+
+                        //cout << "R " << rank << " To " << prev_in_line << " Send " << endl;
+                        int new_color = color;
+                        if (temp[x][y] == EMPTY) {
+                            //cout << "empty" << endl;
+                            A[x][y] = color;
+                            new_color = EMPTY;
+                        }
+                        new_data[0] = new_color;
+                        new_data[1] = x_cells-1;
+                        new_data[2] = y;
+                        //cout << "2 Recv Data Rank " << rank << " color " << new_data[0] << " x" << new_data[1] << " y" << new_data[2] << endl;
+                        MPI_Send(&new_data, 3, MPI_INT, prev_in_line, response_tag, MCW);
+                        MPI_Iprobe(prev_in_line, 2, MCW, &flag, &status);
+                    }
+                }
+
+                MPI_Iprobe(MPI_ANY_SOURCE, response_tag, MCW, &flag, &status);
+                if (flag) {
+                    while (flag) {
+                        MPI_Recv(&data, 3, MPI_INT, next_in_line, 3, MCW, MPI_STATUS_IGNORE);
+                        pending_requests--;
+                        int color = data[0];
+                        int x = data[1];
+                        int y = data[2];
+                        //cout << "Respond Data || Rank " << rank << " color " << data[0] << " x" << data[1] << " y" << data[2] << endl;
+                        A[x][y] = color;
+                        MPI_Iprobe(MPI_ANY_SOURCE, response_tag, MCW, &flag, &status);
+                    }
+                }
+                //TOKEN 
+                MPI_Iprobe(rank - 1, token_tag, MCW, &flag, &status);
+                if (flag and pending_requests==0) {
+                    //cout << "rank " << rank << " recv " << rank - 1 << endl;
+                    MPI_Recv(&work, 1, MPI_INT, rank - 1, token_tag, MCW, MPI_STATUS_IGNORE);
+                    //cout << "rank |*| " << rank << " send to " << (rank + 1) % size << endl;
+                    MPI_Send(&work, 1, MPI_INT, (rank + 1) % size, token_tag, MCW);
+                    break;
+                }
+                //Sleep(1000);
+            }
+            //cout << "cycle" << endl;
             /////////////////////////////////////////////////////////////
-            MPI_Send(&(A[0][0]), XCELLS* YCELLS, MPI_INT, 0, 0, MCW);
-            //MPI_Send(&grid[0][0], 1, MPI_INT, 0, 0, MCW);
+            MPI_Send(&(A[0][0]), x_cells * YCELLS, MPI_INT, 0, 0, MCW);
         }
 
     }
